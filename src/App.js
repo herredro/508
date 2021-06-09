5bfd76592459ed85e35f7d6c3001d6ee8429ee49
Herredro <12672611+herredro@users.noreply.github.com>
2021-06-09 19:55:33 +0200

    hello world

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import pic_side from './img/508_side.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World</h1>
+        <img 
+          className="home_pic" 
+          src={pic_side} 
+          width={"300px"} 
+        />
       </header>
+      <div className='body'>
+      </div>
+      
     </div>
   );
 }
