dc5804ef93c803fae872a2d0aeee35c376bbdcf9
Herredro <12672611+herredro@users.noreply.github.com>
2021-10-03 13:47:21 +0200

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -9,33 +9,75 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: "Hello Europe, I'm a",
-  name: 'rolling digital lab',
-  subtitle: 'that can help you become digital',
-  cta: 'get to know me',
+  title: "Hello Europe, this is a",
+  name: 'rolling digital studio.',
+  subtitle: 'Now, digitization',
+  end: 'comes to you.',
+  cta: 'get digitized',
 };
+// export const heroData = {
+//   title: "Hello Europe, let's",
+//   name: 'get you access',
+//   subtitle: 'to the',
+//   end: 'digital revolution',
+//   cta: 'get to know me',
+// };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'inner.jpeg',
-  paragraphOne: 'Hola Tio',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'hi1.jpg',
+  paragraphOne: 'This is Lukas in the house!',
+  paragraphTwo: 'Boarded with a truck full of equipment and a pool of remote colleagues, he consults small and medium sized companies on how to benefit from the digital revolution.',
+  // paragraphTwo: 'I believe in a decentralised !',
+  paragraphThree: 'Book your appointment remote or on your site.',
+  primaryButton: 'https://drive.google.com/file/d/1HWbQ6a56RyOEk9CG_9oYrrsnhS-oVOT3/view?usp=sharing', // if no resume, the button will not show up
+  secondaryButton: 'mailto:lindnerherrero@gmail.com?subject=Topic%3A%20Book%20Appointment', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: '508_side.jpg',
-    title: '',
-    info: '',
+    img: 'service-data-engineering.jpg',
+    title: 'Data Engineering & Automization',
+    info: 'You already generate data but you can\'t benefit from it? Let\'s make your data work for you!',
     info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    url: 'mailto:lindnerherrero@gmail.com?subject=Topic%3A%20Data%20Engineering',
+  },
+  {
+    id: nanoid(),
+    img: 'service-prototyping.jpg',
+    title: 'Digital Prototyping',
+    info: 'Prototyping digital ideas',
+    info2: 'You have a great idea? Chances are that it will fail due to wrong assumptions and you spend time and money into a dead end. In order to assure you are developing the idea for your clients and not for your bin, you should continously test on your target groups to adapt the development process.',
+    url: 'mailto:lindnerherrero@gmail.com?subject=Topic%3A%20Prototyping',
+  },
+  {
+    id: nanoid(),
+    img: 'service-webdev.jpg',
+    title: 'Web Development',
+    info: 'Website, Online Shop, Blog',
+    info2: ' ',
+    url: 'mailto:lindnerherrero@gmail.com?subject=Topic%3A%20Online%20Shop',
+  },
+  {
+    id: nanoid(),
+    img: 'service-printing.jpg',
+    title: '3D Printing',
+    info: 'You need a screw ',
+    info2: ' ',
+    url: 'mailto:lindnerherrero@gmail.com?subject=Topic%3A%203D%20Printing',
+  },
+  {
+    id: nanoid(),
+    img: 'service-crypto.jpg',
+    title: 'Bitcoin Starter Pack',
+    info: 'Last but not least, I can show you how to buy cryptocurrencies, and everything you need to get started in the exciting world of cryptos.',
+    info2: ' ',
+    url: 'mailto:lindnerherrero@gmail.com?subject=Topic%3A%20Bitcoin%20Starter%20Pack',
   },
 ];
+
 
 // CONTACT DATA
 export const contactData = {
